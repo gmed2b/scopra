9f24ca68e0457257f0ef58f0e85bf5ba8ed03e98
Mehdi Ghoulam <aznoggfr@gmail.com>
2022-11-25 12:41:07 +0100

    Passage de https vers http

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+	require('dotenv').config()
 }
-const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
 
-const path = require('path');
-const fs = require('fs');
-const https = require('https');
-const express = require('express');
-const app = express();
+const path = require('path')
+const fs = require('fs')
+const compression = require('compression')
+const http = require('http')
+const express = require('express')
+const app = express()
+const PORT = 8010
 
-app.use(express.json());
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')));
-app.use('/img', express.static(path.join(__dirname, 'public/img')));
-app.use(express.static('public'));
+app.use(compression())
+app.use(express.json())
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')))
+app.use('/img', express.static(path.join(__dirname, 'public/img')))
+app.use(express.static('public'))
 
-https.createServer({
-  key: fs.readFileSync('.privkey.pem'),
-  cert: fs.readFileSync('.cert.pem')
-}, app).listen(3000);
+const server = http.createServer(app)
+server.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// https.createServer({
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem')
+// }, app).listen(3000);
